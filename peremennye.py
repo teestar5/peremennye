3eddca18efd8a6e0301f314be22ba440984ec231
teestar5 <147434843+teestar5@users.noreply.github.com>
2024-10-17 12:18:49 +0300

    Add files via upload
    Практическое задание по работе в Pycharm - "Переменные".
    
    
    
    Цель: научиться правильно называть переменные и взаимодействовать с ними.
    
    
    
    Задача:
    
    Напишите 4 переменных которые буду обозначать следующие данные:
    
    Количество выполненных ДЗ (запишите значение 12)
    Количество затраченных часов (запишите значение 1.5)
    Название курса (запишите значение 'Python')
    Время на одно задание (вычислить используя 1 и 2 переменные)
    Выведите на экран(в консоль), используя переменные, следующую строку:
    
    Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
    Примечания:
    
    Для вывода значений на экран используйте функцию print(a, b, c), где a, b, c - данные которые выводятся на экран.
    Для названия переменных используйте только: латинские буквы, не начинайте запись с числа, пишите в змеином регистре(разделяя слова символом '_')
    Пробелы между словами и символами ':' ',' можно ставить на своё усмотрение.

---
+++ peremennye.py
@@ -0,0 +1,7 @@
+
+course_title = "Курс: Pyton,"
+home_work = 12
+hours_spent = 1.5
+average_execution_time = hours_spent/home_work
+print(course_title, "всего задач:",home_work,",", "затрачено часов:",hours_spent,",", "среднее время выполнения:",average_execution_time)
+
